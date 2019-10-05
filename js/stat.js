7ab37b3a3dfaea403972188f51327108aa371cd3
Jullka <dj-1@ukr.net>
2019-10-05 23:05:09 +0300

    Добавляет исправления Тревиса

--- js/stat.js
+++ js/stat.js
@@ -4,17 +4,17 @@ var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
 var CLOUD_X = 100;
 var CLOUD_Y = 10;
-var GAP = 10;
+var GAP = 15;
 var SPACE_BETWEEN = 50;
 var BAR_WIDTH = 40;
 var BAR_HEIGHT_MAX = 150;
 
-var renderCloud = function(ctx, x, y, color) {
+var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
-var getMaxElement = function(arr) {
+var getMaxElement = function (arr) {
   var maxElement = arr[0];
 
   for (var i = 1; i < arr.length; i++) {
@@ -25,15 +25,15 @@ var getMaxElement = function(arr) {
   return maxElement;
 };
 
-window.renderStatistics = function(ctx, players, times) {
+window.renderStatistics = function (ctx, players, times) {
   renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
 
-  ctx.font = "16px PT Mono";
+  ctx.font = '16px PT Mono';
   ctx.textBaseLine = 'handing';
   ctx.fillStyle = 'rgb(0, 0, 0)';
-  ctx.fillText('Ура вы победили!', CLOUD_X + SPACE_BETWEEN, CLOUD_Y + SPACE_BETWEEN);
-  ctx.fillText('Список результатов:', CLOUD_X + SPACE_BETWEEN, CLOUD_Y + SPACE_BETWEEN + GAP * 2);
+  ctx.fillText('Ура вы победили!', CLOUD_X + SPACE_BETWEEN, CLOUD_Y + GAP * 2);
+  ctx.fillText('Список результатов:', CLOUD_X + SPACE_BETWEEN, CLOUD_Y + SPACE_BETWEEN);
 
   var maxTime = getMaxElement(times);
 
@@ -42,7 +42,7 @@ window.renderStatistics = function(ctx, players, times) {
 
     var barHeight = BAR_HEIGHT_MAX * times[i] / maxTime;
 
-    var positionX = CLOUD_X + SPACE_BETWEEN + GAP + (GAP + BAR_WIDTH + SPACE_BETWEEN) * i;
+    var positionX = CLOUD_X + SPACE_BETWEEN + (GAP + BAR_WIDTH + SPACE_BETWEEN) * i;
 
     ctx.fillText(players[i], positionX, CLOUD_Y + CLOUD_HEIGHT - GAP);
     ctx.fillText(Math.ceil(times[i]), positionX, CLOUD_HEIGHT - barHeight - GAP);
@@ -51,9 +51,6 @@ window.renderStatistics = function(ctx, players, times) {
 
     ctx.fillStyle = (players[i] === 'Вы') ? colorBar : 'hsl(240,' + Math.floor(Math.random() * 100) + '% , 50%)';
 
-    ctx.fillRect(positionX, CLOUD_Y + BAR_HEIGHT_MAX - barHeight + 100,  BAR_WIDTH, barHeight);
+    ctx.fillRect(positionX, CLOUD_Y + BAR_HEIGHT_MAX - barHeight + SPACE_BETWEEN + GAP, BAR_WIDTH, barHeight);
   }
 };
-
-
-
